685018afa44dd3ec0308e6886b1a21d191c4cbb3
Eugene-Khonich <ehonich@gmail.com>
2025-01-30 14:41:47 +0200

    add avlidate

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const setupServer = () => {
   const app = express();
+  // app.use((req, res, next) => {
+  //   console.log(`Incoming request: ${req.method} ${req.url}`);
+  //   next();
+  // });
   app.use(cors());
   app.use(express.json());
   app.use(
