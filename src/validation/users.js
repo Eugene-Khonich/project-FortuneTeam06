21d447f7c5e9589278a33c05c7cb6147d1faf469
Eugene-Khonich <ehonich@gmail.com>
2025-02-07 19:40:51 +0200

    little fix

--- src/validation/users.js
+++ src/validation/users.js
@@ -4,5 +4,5 @@ export const updateUserSchema = Joi.object({
   name: Joi.string().min(3).max(32),
   email: Joi.string().email(),
   gender: Joi.string().valid('male', 'female'),
-  dailyNorm: Joi.number().min(1).max(15000).integer(),
+  dailyNorm: Joi.number().min(1).max(1500).integer(),
 });
