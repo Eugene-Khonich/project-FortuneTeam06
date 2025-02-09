12beafb718fe2b2c97b31bfce8e420c87579e6bf
Eugene-Khonich <ehonich@gmail.com>
2025-02-09 19:03:45 +0200

    fix

--- src/routers/water.js
+++ src/routers/water.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { validateBody, validateParams } from '../middlewares/validateBody.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { validateParams } from '../middlewares/validateParams.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   addWaterController,
